666a0d0f3812975b2c12673f0590a0c11ed14b70
CJ Manzo <115611137+cjmanzo@users.noreply.github.com>
2023-09-22 12:08:50 +1200

    rock_paper_scissors project.py

--- rock_paper_scissors project.py
+++ rock_paper_scissors project.py
@@ -4,6 +4,10 @@ myScore = 0
 computerScore = 0
 
 while True:
+    """
+    User is asked to enter input from the given selection.
+    Then follows by an option for quitting
+    """
     userInput = input("Enter rock, paper, or scissors: ").lower()
     if userInput == "quit":
         print ("I resign! Game Over")
@@ -12,10 +16,16 @@ while True:
         print("Please choose a correct input\n")
         continue
 
+    """
+    Computer imput is generated through getting a random choice from the same selection.
+    """
     computerInput = random.choice(["rock", "paper", "scissors"])
     print (f"Computer: {computerInput}\n")
 
 
+    """
+    Deciding factor and incrementing score
+    """
     if computerInput == userInput:
         print ("It's a tie!")
     elif userInput == "rock" and computerInput == "scissors":
@@ -30,7 +40,10 @@ while True:
     else:
         print ("You Lost!")
         computerScore += 1
-    
+
+    """
+    Outcome, Game over whoever reaches 5 first.
+    """
     print(f"my score: {myScore} computer score: {computerScore}\n\n")
     if myScore == 5 or computerScore == 5:
         print("Game Over!")
